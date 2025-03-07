c292d82707d1fd1322ff40db1bb389b5ca71987d
Mikoilov <134260971+Mikoilov@users.noreply.github.com>
2025-03-07 10:48:31 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "ssh_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCd0uDqGgxI/5b4WvECc/70h+TtN8q9wIi4bdvzXrfI8tlm4xO/qeQeYlSwHE6y3BTWUzkn4fThcD0rBxrA0/DpJu6d0zjmQh7BIDOvwfO4m1VusbgvRUyBImBN4F+ihrvmzyOIWmnEimnXxZRcWVWsqlIzB4QqYIXNWY0LvVhqwrz4RXWMBibcoFZ4Yvqadpg4VStcmHPpQK73v8uSszsl5wTQph5Cvk3u8og9xjEob+Neeo9WiZBZmyqNqh3uDcb59tKudpe+KB0U+Ohiw0GeVylGPsNLYEfNDsZ7UeezhybzK3sCc/bBgZPd9lz96gFOC80rdUS/f+HNZQjk6u5vhQIn5eOzctv96rVXofZluha0SfufzOEYK8sxb7lI23F+45ldVo+BfbUrWdkhOZ4OzeMIM4QfR46uOXjAKOI/zZ6V5Yrkl/qAjrxFubvrcblmyVZyH8Y8VDksABhPBqDqQaHq8MQBVkyoMJ1lM1bTYoOVSGr1GcZNiTX07eSwfgFs/qldfju9xonegJR2FXOyHbzvAvX7tqTbIAr1+boUIsEuh5SY2KCI1DvkSqKOgNunhuKKg05ZEUUVBvg/u6ErXjrG7QtObISYBk2IIxJlk+BSJ0wkakkUvP1ILbmLTQDPMJINNKwJNCG9l9XoswG424C9vTk3d6AQLGGLKimzKw== root@pve"
+}
+variable "proxmox_host" {
+  default = "proxmox1"
+}
+variable "template_name" {
+  default = "8001"
+}
